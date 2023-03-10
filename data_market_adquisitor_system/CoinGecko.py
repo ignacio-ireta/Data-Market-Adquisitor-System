6f1602c2089757e54904b994c9e2a38b2e04df3a
Ignacio Ireta <ignacio.ireta@outlook.com>
2023-03-10 02:46:58 -0600

    Cleaned code a bit further, created object that returns whole OHLCV information on interest asset, and saves to CSV

--- data_market_adquisitor_system/CoinGecko.py
+++ data_market_adquisitor_system/CoinGecko.py
@@ -9,29 +9,56 @@ def datefromtimestamp(timestamp):
     return date
 
 
-# Set the URL parameters for the desired coin/exchange pair/timeframe
-coin_id = 'ripple'
-against = 'usd'
-timeframe = 'max'
+def create(coin_id, against, timeframe):
+    # Create a Pandas DataFrame that contains OHCL values for coin_id valued against another currency
+    # in a given timeframe
+    # Set the API endpoint URL for OHLC
+    historical_url = f'https://api.coingecko.com/api/v3/coins/{coin_id}/ohlc?vs_currency={against}&days={timeframe}'
+    # Timeframe can be 1/7/14/30/90/180/365/max
+    # Make a get request and save the response
+    historical_response = requests.get(historical_url)
+
+    # Verify API call was successful
+    if historical_response.status_code == 200:
+
+        # Create DataFrame from JSON response
+        historical_df = pd.DataFrame(historical_response.json())
+        # Rename DataFrame columns
+        historical_df.rename(columns={0: 'Unix_timestamp', 1: 'Open', 2: 'High', 3: 'Low', 4: 'Close'}, inplace=True)
+        # Convert Unix timestamp to MM-DD-YYYY date
+        historical_df['Date'] = (historical_df['Unix_timestamp'] / 1000).apply(datefromtimestamp)
+        # Retrieve start and end dates for later volume call
+        first = historical_df.Unix_timestamp.iloc[0] / 1000
+        last = historical_df.Unix_timestamp.iloc[-1] / 1000
+        # Drop Unix timestamp
+        historical_df = historical_df.drop(columns=['Unix_timestamp'])
+
+    else:
+
+        return 'Failed to get historical data from the API'
+
+    volume_url = f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart/range?vs_currency={against}&from={first}&to={last}"
+    # Make a get request and save the response
+    volume_response = requests.get(volume_url)
+
+    # Verify API call was successful
+    if volume_response.status_code == 200:
+
+        # Create DataFrame from JSON response
+        volume_df = pd.DataFrame(volume_response.json())
+        # Keep only the pertinent information
+        volume_df = pd.DataFrame(volume_df['total_volumes'].to_list(), columns=['Unix_timestamp', 'Volume'])
+        # Convert Unix timestamp to MM-DD-YYYY date
+        volume_df['Date'] = (volume_df['Unix_timestamp'] / 1000).apply(datefromtimestamp)
+        # Drop Unix timestamp
+        volume_df = volume_df.drop(columns=['Unix_timestamp'])
+
+    else:
+
+        return 'Failed to get volume data from the API'
+
+    return historical_df.join(volume_df.set_index('Date'), on='Date')
 
 
-# Set the API endpoint URL and parameters
-url = f'https://api.coingecko.com/api/v3/coins/{coin_id}/ohlc?vs_currency={against}&days={timeframe}'
-
-
-# Make the API call and get the response
-response = requests.get(url)
-
-
-# Verify API call was successful
-if response.status_code == 200:
-    coin_df = pd.DataFrame(response.json())
-else:
-    print('Failed to get data from the API')
-
-# Rename DataFrame columns
-coin_df.rename(columns={0: 'Unix_timestamp', 1: 'Open', 2: 'High', 3: 'Low', 4: 'Close'}, inplace=True)
-# Convert Unix timestamp to MM-DD-YYYY date
-coin_df['Date'] = (coin_df['Unix_timestamp']/1000).apply(datefromtimestamp)
-# Drop Unix timestamp column
-coin_df.drop(columns='Unix_timestamp')
+whole_df = create('ripple', 'usd', 'max')
+whole_df.to_csv(f'coin_OHLCV.csv')
